c189bc3c6b74b2fffee73b1e190209036e0235b6
kquach229 <kquach229@gmail.com>
2025-03-09 20:42:58 -0400

    additional tweaks on news page

--- app/platform/news/page.tsx
+++ app/platform/news/page.tsx
@@ -2,7 +2,12 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import ReusablePaper from '@/app/components/ReusablePaper';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from '@/components/ui/card';
 import Image from 'next/image';
 import { ellipse } from '@/src/lib/utils';
 
@@ -24,27 +29,42 @@ const NewsPage = () => {
   });
 
   console.log(data);
+
   return (
     <div>
       <ReusablePaper>Crypto News</ReusablePaper>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
         {data &&
-          data.Data.map((news) => (
-            <Card key={news.ID}>
-              <CardContent className='mx-auto'>
-                <div className='h-[200] w-auto'>
-                  <Image
-                    src={news.IMAGE_URL}
-                    height={300}
-                    width={500}
-                    alt={news.TITLE}
-                  />
-                </div>
-                <h1 className='font-bold mt-5 text-left'>{news.TITLE}</h1>
-                <div className='mt-5'>{ellipse(news.BODY, 155)}</div>
-              </CardContent>
-            </Card>
-          ))}
+          data.Data.map((news) => {
+            const date = new Date(news.PUBLISHED_ON * 1000).toLocaleDateString(
+              'en-US'
+            );
+
+            return (
+              <Card key={news.ID}>
+                <CardContent className='mx-auto'>
+                  <div className='h-[200] w-auto'>
+                    <Image
+                      src={news.IMAGE_URL}
+                      height={300}
+                      width={500}
+                      alt={news.TITLE}
+                    />
+                  </div>
+                  <div>
+                    <h1 className='font-bold mt-5 text-left'>{news.TITLE}</h1>
+                    <div className='mt-5'>{ellipse(news.BODY, 155)}</div>
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <div className='flex justify-between w-full'>
+                    <h5>Learn More</h5>
+                    <h5>{date}</h5>
+                  </div>
+                </CardFooter>
+              </Card>
+            );
+          })}
       </div>
     </div>
   );
