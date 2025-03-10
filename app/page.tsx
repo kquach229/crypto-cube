244e307133bebbed1a5e46255623b2b4eea2c3b7
kquach229 <kquach229@gmail.com>
2025-03-10 18:51:07 -0400

    add  lottie file and loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 'use client';
 
-import { ModeToggle } from '@/components/ModeToggle';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { useTheme } from 'next-themes';
-import { useState } from 'react';
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className='h-100 mx-auto w-full text-center flex flex-col justify-center'>
+    <div className='relative h-100 mx-auto w-full text-center flex flex-col justify-center'>
       <h1 className='text-[5rem]'>
-        Stay Ahead With <h5 className='font-extrabold'>CryptoCube</h5>
+        Stay Ahead With <h5 className='font-extrabold'>CryptoCube </h5>
+        <DotLottieReact
+          className='absolute top-[25rem] md:top-[5rem] md:right-[0rem]'
+          color='oklch(90.74% 0.0267 138.3)'
+          src='/cube-lottie'
+          loop
+          autoplay
+          width={300}
+          height={300}
+          draggable
+        />
       </h1>
+
       <p className='mt-5'>
         Track Trends in Crypto and NFTs to make informed decisions
       </p>
